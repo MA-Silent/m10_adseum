748396f828517d41be03e48533201325f3de9b84
MA-Silent <38739@ma-web.nl>
2025-11-23 20:31:05 +0100

    gave myself a example of prisma in Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
 "use server"
 import { getTranslations } from "next-intl/server";
+import { prisma } from "../lib/prisma";
+
+const users = await prisma.user.findMany();
 
 export default async function Header() {
   const t = await getTranslations('Header');
@@ -8,7 +11,7 @@ export default async function Header() {
     <div className="fixed top-0 right-2 left-2 text-secondary">
       <div className="bg-black/60 w-full p-4 justify-between flex rounded">
         <div>{t('title')}</div>
-        <div>Links</div>
+        <div className="flex gap-4">{users.map((user) => <div key={user.id}>{user.name}</div>)}</div>
       </div>
     </div>
   )
